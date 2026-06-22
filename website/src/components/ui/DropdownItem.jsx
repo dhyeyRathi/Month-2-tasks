868015a2af9cccc9a1d80c3a90e42e747d60f630
Dhyey Rathi <dhyeyrathi7@gmail.com>
2026-06-22 10:58:19 +0530

    Update: Navbar state persistence

--- website/src/components/ui/DropdownItem.jsx
+++ website/src/components/ui/DropdownItem.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const DropdownItem = ({ children , ...props }) => {
+const DropdownItem = ({ children , className},...props ) => {
   return (
     <button
-      className={`flex items-center bg-primary/30  backdrop-blur-md justify-left gap-4 w-70 py-5 px-8 hover:bg-secondary/80 hover:text-primary  text-secondary font-bold ${props.className}`}
+      className={`flex items-center bg-primary/30  backdrop-blur-md justify-left gap-4 w-70 py-5 px-8 hover:bg-secondary/80 hover:text-primary  text-secondary font-bold ${className}`}
       {...props}
     >
       {children}
