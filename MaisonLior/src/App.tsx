18fb92ee474b0dc8e09d6fdb369ff1ca37fb96d1
Dhyey Rathi <dhyeyrathi7@gmail.com>
2026-06-26 11:41:24 +0530

    Bug fixes: Build erros

--- MaisonLior/src/App.tsx
+++ MaisonLior/src/App.tsx
@@ -1,14 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 
 import "./index.css";
-import Navbar from "./components/layout/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
 import Portfolio from "./pages/Portfolio";
 import Journal from "./pages/Journal";
 import Contact from "./pages/Contact";
-import Footer from "./components/layout/Footer";
 import CouplePage from "./pages/CouplePage";
 import JournalPage from "./pages/JournalPage";
 import ScrollToTop from "./components/layout/ScrollToTop";
