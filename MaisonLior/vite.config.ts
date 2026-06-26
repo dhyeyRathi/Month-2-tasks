9f3254d11458d2827f9bf889a229c6d670efee17
Dhyey Rathi <dhyeyrathi7@gmail.com>
2026-06-26 12:13:02 +0530

    bug fixes: sitemap.generated

--- MaisonLior/vite.config.ts
+++ MaisonLior/vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import sitemap from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),sitemap({
-      hostname: "https://maisonlior-dhyey.vercel.app/",
-    })],
+  plugins: [react(), tailwindcss()],
 })
