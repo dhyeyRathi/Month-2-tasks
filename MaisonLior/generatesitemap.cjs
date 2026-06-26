35c67d7a6f7d60c173f37db400b483eec3b87944
Dhyey Rathi <dhyeyrathi7@gmail.com>
2026-06-26 17:11:47 +0530

    update: nested sitemaps

--- MaisonLior/generatesitemap.cjs
+++ MaisonLior/generatesitemap.cjs
@@ -2,18 +2,15 @@ const fs = require("fs");
 const couples = require("./src/data/couples.json");
 const journals = require("./src/data/journals.json");
 
-
-
-
-const baseUrl = "https://maison-lior.vercel.app";
+const baseUrl = "https://maisonlior-dhyey.vercel.app";
 
 const staticRoutes = [
-  "",
-  "/about",
-  "/services",
-  "/portfolio",
-  "/journal",
-  "/contact",
+  { path: "", priority: 1.0 },
+  { path: "/about", priority: 0.8 },
+  { path: "/services", priority: 0.9 },
+  { path: "/portfolio", priority: 0.9 },
+  { path: "/journal", priority: 0.8 },
+  { path: "/contact", priority: 0.7 },
 ];
 
 const portfolioRoutes = couples.map(
@@ -24,15 +21,41 @@ const journalRoutes = journals.map(
   (item) => `/journal/${item.url}`
 );
 
+
 const routes = [
   ...staticRoutes,
-  ...portfolioRoutes,
-  ...journalRoutes,
+];
+
+const indiJournal =[
+  ...journalRoutes
+];
+
+const indiPortfolio =[
+  ...portfolioRoutes
 ];
 
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${routes
+  .map(
+    (route) => `
+  <url>
+    <loc>${baseUrl}${route.path}</loc>
+    <priority>${route.priority}</priority>
+  </url>`
+  )
+  .join("")}
+</urlset>`;
+
+fs.writeFileSync("./public/sitemap.xml", sitemap);
+
+// JOURNAL SITEMAP
+
+
+
+const JournalSitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${indiJournal
   .map(
     (route) => `
   <url>
@@ -42,4 +65,21 @@ ${routes
   .join("")}
 </urlset>`;
 
-fs.writeFileSync("./public/sitemap.xml", sitemap);
+fs.writeFileSync("./public/journal/sitemap.xml", JournalSitemap);
+
+// PORTFOLIO SITEMAP
+
+
+const PortfolioSitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${indiPortfolio
+  .map(
+    (route) => `
+  <url>
+    <loc>${baseUrl}${route}</loc>
+  </url>`
+  )
+  .join("")}
+</urlset>`;
+
+fs.writeFileSync("./public/portfolio/sitemap.xml", PortfolioSitemap);
